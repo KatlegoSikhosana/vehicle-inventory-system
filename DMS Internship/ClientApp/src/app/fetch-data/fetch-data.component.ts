8daa3afb60b161a30febc78857d73d7be5079944
Matthew Jeffrey <matthewjeffrey@users.noreply.github.com>
2024-01-18 22:48:28 +0200

    Update Button (still in progress)

--- DMS Internship/ClientApp/src/app/fetch-data/fetch-data.component.ts
+++ DMS Internship/ClientApp/src/app/fetch-data/fetch-data.component.ts
@@ -1,6 +1,8 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { VehicleModel, VehicleInfo } from '../VehicleInfo';
 import { VehicleService } from '../vehicle.service';
+import { HttpClient } from '@angular/common/http';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-fetch-data',
@@ -9,8 +11,8 @@ import { VehicleService } from '../vehicle.service';
 export class FetchDataComponent implements OnInit {
   public vehicleModels: VehicleModel[] = [];
 
-  constructor(private vehicleService: VehicleService) {
-
+  constructor(private vehicleService: VehicleService, private http: HttpClient) {
+    // ...
   }
 
   ngOnInit() {
@@ -24,22 +26,65 @@ export class FetchDataComponent implements OnInit {
     );
   }
 
-  vehicleId = { id: 0, series: ' ', priceInclusive: 0, price: 0 };
+  vehicleWork = {id: 0 ,series: ' ', priceInclusive: 0, price: 0 }; //vehicleWork is a placeholder name doen't mean anything just trying to change the name from the one on VehicleInfo.ts
 
-  searchVehicle() {
+  updateVehicleForm(f: any) {
+    console.log("I am here");
+    console.log(f);
+  }
+
+  searchVehicles() {
     const filteredVehicle: VehicleModel = {
-      id: this.vehicleId.id,
-      series: this.vehicleId.series,
-      priceInclusive: this.vehicleId.priceInclusive,
-      price: this.vehicleId.price
-    }
+      id: this.vehicleWork.id,
+      series: this.vehicleWork.series,
+      priceInclusive: this.vehicleWork.priceInclusive,
+      price: this.vehicleWork.price
+    }; 
 
     this.vehicleService.searchVehicles(filteredVehicle.id).subscribe(() => {
-
+      // ..
     });
   };
 
-  updateVehicle() {
+  //inputValues = { Make: ' ', Model: ' ', Price: 0 };
 
-  }
-}
+  //updateVehicle() {
+  //  const updatedVehicle: VehicleModel = {
+  //    id: 0,
+  //    series: this.inputValues.Make + this.inputValues.Model,
+  //    priceInclusive: this.inputValues.Price,
+  //    price: this.inputValues.Price
+  //  };
+  //}
+
+
+  //let vehicleData = {
+  //  id: data.vehicleId,
+  //  series: data.Make + data.Model,
+  //  priceInclusive: data.Price,
+  //  price: data.Prices
+
+  //vehicleUpdate = {vehicleId: 0 ,Make: ' ', Model: ' ', Price: 0 };
+
+  updateVehicle() {
+    console.log("Blah blah blah");
+    //const updatedVehicle: VehicleModel = {
+    //  id: this.vehicleUpdate.vehicleId,
+    //  series: this.vehicleUpdate.Make + this.vehicleUpdate.Model,
+    //  priceInclusive: this.vehicleUpdate.Price,
+    //  price: this.vehicleUpdate.Price
+    //};
+
+    //this.vehicleService.updateVehicle(updatedVehicle.id, updatedVehicle)
+    //  .subscribe(response => {
+    //    console.log(response)
+    //});
+  };
+
+  deleteVehicle() {
+    this.vehicleService.deleteVehicle(this.vehicleWork.id)
+      .subscribe(response => {
+        console.log(response)
+      });
+  };
+};
